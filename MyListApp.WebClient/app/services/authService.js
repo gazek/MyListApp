91a48e78289c3160fbe4789930d7794f9f329425
Greg Waliczek <gregory.waliczek@gmail.com>
2016-08-25 23:35:53 -0700

    added uthservice and index controller

---
+++ MyListApp.WebClient/app/services/authService.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+app.factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService) {
+
+    //TODO: move baseUrl to a config
+    var baseUrl = 'http://localhost:62357';
+    var authServiceFactory = {};
+
+    //Login
+
+    //Logout
+
+
+
+
+}]);
