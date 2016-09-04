f47e6802988de313cc91ca3bafd7c4e055f9ac71
Greg Waliczek <gregory.waliczek@gmail.com>
2016-09-03 23:20:40 -0700

    General cleanup after moving list layout to component

--- MyListApp.WebClient/app/controllers/homeController.js
+++ MyListApp.WebClient/app/controllers/homeController.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-app.controller('homeController', ['$scope', '$location', 'homeService', 'authService', function ($scope, $location, homeService, authService) {
+app.controller('homeController', ['$scope', 'homeService', function ($scope, homeService) {
 
     $scope.getAllLists = function () {
         homeService.getAll().then(function (response) {
@@ -7,61 +7,6 @@ app.controller('homeController', ['$scope', '$location', 'homeService', 'authSer
         });
     };
 
-    $scope.lists = $scope.getAllLists();
-
-    var _items = [];
-    for (var i = 1; i <= 6; i++) {
-        _items.push({
-            text: 'Item ' + i,
-            value: i
-        });
-    }
-    $scope.items = _items;
-
-    // redirect to auth route if not currently authenticated
-    if (!authService.authentication.isAuth) {
-        authService.logout();
-        $location.path('/auth');
-    }
-
-    var tmpList = [];
-
-    for (var i = 1; i <= 6; i++) {
-        tmpList.push({
-            text: 'Item ' + i,
-            value: i
-        });
-    }
-
-    $scope.list = tmpList;
-
-
-    $scope.sortingLog = [];
-
-    $scope.sortableOptions = {
-        helper:function(e, ui) {  
-            ui.children().each(function() {  
-                $(this).width($(this).width());  
-            });  
-            return ui;  
-        },
-        update: function (e, ui) {
-            console.log("update");
-
-            var logEntry = tmpList.map(function (i) {
-                return i.value;
-            }).join(', ');
-            $scope.sortingLog.push('Update: ' + logEntry);
-        },
-        stop: function (e, ui) {
-            console.log("stop");
-
-            // this callback has the changed model
-            var logEntry = tmpList.map(function (i) {
-                return i.value;
-            }).join(', ');
-            $scope.sortingLog.push('Stop: ' + logEntry);
-        }
-    };
+    $scope.lists = $scope.getAllLists(); 
 
 }]);
