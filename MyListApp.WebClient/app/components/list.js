0ce337aeff037f1c0ac9ec82fe737166839e21d6
Greg Waliczek <gregory.waliczek@gmail.com>
2016-09-03 23:18:25 -0700

    Basic list panel layout created as component

---
+++ MyListApp.WebClient/app/components/list.js
@@ -0,0 +1,7 @@
+﻿app.component('listComponent', {
+    bindings: {
+        list: '<',
+    },
+    templateUrl: 'app/components/list.html',
+    controller: listController
+});
---
+++ MyListApp.WebClient/app/components/listController.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+function listController($http, $scope) {
+
+    
+    $scope.list = this.list;
+
+    $scope.sortingLog = [];
+    $scope.sortableOptions = {
+        handle: '.listItemSortHandle',
+        helper: function (e, ui) {
+            ui.children().each(function () {
+                $(this).width($(this).width());
+            });
+            return ui;
+        },
+        update: function (e, ui) {
+            var logEntry =  $scope.list.items.map(function (i) {
+                return i.id;
+            }).join(', ');
+            console.log("Update: " + logEntry);
+            $scope.sortingLog.push('Update: ' + logEntry);
+        }
+    };
+
+}
