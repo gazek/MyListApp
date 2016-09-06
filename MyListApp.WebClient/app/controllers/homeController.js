0c8dedba2c3bf4f6819ba367bb83a60c691b70cc
Greg Waliczek <gregory.waliczek@gmail.com>
2016-09-05 21:14:06 -0700

    added create,delete and update http funtions

--- MyListApp.WebClient/app/controllers/homeController.js
+++ MyListApp.WebClient/app/controllers/homeController.js
@@ -1,12 +1,65 @@
 ﻿'use strict';
-app.controller('homeController', ['$scope', 'homeService', function ($scope, homeService) {
+app.controller('homeController', ['$scope', 'listRepositoryService', 'listItemRepositoryService', function ($scope, listRepositoryService, listItemRepositoryService) {
 
-    $scope.getAllLists = function () {
-        homeService.getAll().then(function (response) {
+    $scope.retrieveAll = function () {
+        listRepositoryService.retrieveAll().then(function (response) {
             $scope.lists = response.data;
+        }, function (response) {
+            if (response.status !== 401) {
+                $scope.lists = [];
+                alert('Failed to retrieve lists');
+            }
         });
     };
 
-    $scope.lists = $scope.getAllLists(); 
+    $scope.onCreateToDoList = function () {
+        $scope.onCreateList(0);
+    }
+
+    $scope.onCreateToBuyList = function () {
+        $scope.onCreateList(1);
+    }
+
+    $scope.onCreateList = function (type) {
+        var listNum = $scope.lists.length+1;
+        var postList = {
+            ownerId: 'fake', // API will supply value
+            name: 'New List' + listNum,
+            type: type,
+            items: [],
+            sharing: []
+        };
+        listRepositoryService.create(postList).then(function (response) {
+            if (typeof(response) === 'object' && 'id' in response) {
+                $scope.lists.push(response);
+            } else {
+                alert('Failed to create new list.\n' + response);
+            }
+        });
+    };
+
+    $scope.onDeleteList = function (index) {
+        var id = $scope.lists[index].id;
+        listRepositoryService.delete(id).then(function (response) {
+            if (response) {
+                $scope.lists.splice(index, 1);
+            } else {
+                alert('Failed to delete list\n'+response);
+            }
+        });
+    };
+
+    $scope.onUpdateList = function (index) {
+        var list = $scope.lists[index];
+        console.log('list update home controller');
+        console.log(list);
+        listRepositoryService.update(list).then(function (response) {
+            if (!response) {
+                alert('Failed to delete list\n' + response);
+            }
+        });
+    };
+
+    $scope.lists = $scope.retrieveAll();
 
 }]);
