737bdd22a12076a2c3c96a4b3c2238146c72506a
gwalicze <gwalicze@GWALICZE-MOBL3>
2016-09-07 16:11:07 -0700

    connectedWith list sort

--- MyListApp.WebClient/app/controllers/homeController.js
+++ MyListApp.WebClient/app/controllers/homeController.js
@@ -4,6 +4,19 @@ app.controller('homeController', ['$scope', 'listRepositoryService', 'listItemRe
     $scope.retrieveAll = function () {
         listRepositoryService.retrieveAll().then(function (response) {
             $scope.lists = response.data;
+            $scope.numCols = 4;
+            $scope.colClass = "col-sm-" + Math.floor(12/$scope.numCols);
+            $scope.listCols = [];
+            for (var i = 0; i < $scope.numCols; i++) {
+                $scope.listCols[i] = [];
+            }
+            for (var l in $scope.lists) {
+                console.log('l: ' + l);
+                console.log('position: ' + $scope.lists[l].position);
+                console.log('mod: ' + $scope.lists[l].position % $scope.numCols);
+                $scope.listCols[$scope.lists[l].position % $scope.numCols].push($scope.lists[l]);
+            }
+
             // create an array of lists accessible by ID
             $scope.listLookup = {};
             for (var l in $scope.lists) {
@@ -35,7 +48,7 @@ app.controller('homeController', ['$scope', 'listRepositoryService', 'listItemRe
     $scope.onCreateList = function (type) {
         var listNum = $scope.lists.length+1;
         var newList = {
-            ownerId: 'fake', // API will supply value
+            ownerId: 'fake', // will be assigned by API based on token
             name: 'New List' + listNum,
             type: type,
             items: [],
@@ -74,7 +87,7 @@ app.controller('homeController', ['$scope', 'listRepositoryService', 'listItemRe
     $scope.onCreateItem = function (listId) {
         var newItem = {
             ListId: listId,
-            CreatorId: 'fake', // will be assigned by API
+            CreatorId: 'fake', // will be assigned by API based on token
             Name: 'New item',
             position: $scope.listLookup[listId].items.lenth
         };
@@ -82,6 +95,7 @@ app.controller('homeController', ['$scope', 'listRepositoryService', 'listItemRe
         listItemRepositoryService.create(newItem).then(function (response) {
             if (typeof (response) === 'object' && 'id' in response) {
                 $scope.listLookup[response.listId].items.push(response);
+                $scope.listItemLookup[response.id]= response;
             } else {
                 alert('Failed to create new list.\n' + response);
             }
@@ -92,6 +106,41 @@ app.controller('homeController', ['$scope', 'listRepositoryService', 'listItemRe
         var item = $scope.listItemLookup[itemId];
         listItemRepositoryService.update(item);
     }
+
+    function sortableUpdate(e) {
+        var index = e.target.id.substr(7);
+        for (var l in $scope.listCols[index]) {
+            var expectedPosition = parseInt(index + l * $scope.numCols);
+            console.log('expectedPosition: ' + expectedPosition);
+            console.log('position: ' + $scope.listCols[index][l].position);
+            if ($scope.listCols[index][l].position != expectedPosition) {
+                $scope.listCols[index][l].position = expectedPosition;
+                listRepositoryService.update($scope.listCols[index][l]);
+            }
+        }
+    }
+
+    $scope.sortableOptions = {
+        connectWith: '.list-group',
+        handle: '.listSortHandle',
+        helper: function (e, ui) {
+            ui.children().each(function () {
+                $(this).width($(this).width());
+            });
+            return ui;
+        },
+        receive: function (e, ui) {
+            console.log('homeController: ui-sortable receive event '+e.target.id);
+            sortableUpdate(e);
+        },
+        remove: function (e, ui) {
+            console.log('homeController: ui-sortable remove event ' + e.target.id);
+        },
+        stop: function (e, ui) {
+            console.log('homeController: ui-sortable stop event ' + e.target.id);
+            sortableUpdate(e);
+        }
+    };
     
     // grab all lists readble by the user
     $scope.retrieveAll();
