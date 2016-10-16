7f11bcae5a19ffd7afae31c5f0e9d3644f601f4b
White Rabbit <White Rabbit@YOGA>
2016-10-15 22:09:17 -0700

    Add file to project

--- MyListApp.Api/Migrations/Configuration.cs
+++ MyListApp.Api/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace MyListApp.Api.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(Data.Context.AppDbContext context)
